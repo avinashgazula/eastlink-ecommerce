d5d25f517dcb1de66999d574fe4c6d0ab54ddcd3
avinashgazula <avinashgazula@gmail.com>
2025-10-07 22:29:05 -0300

    change product detail to dynamic

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,29 +3,26 @@ import { Header } from "@/components/main/Header";
 import { ProductDetail } from "@/components/products/ProductDetail";
 import { notFound } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 const BASE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXT_PUBLIC_SITE_URL
 
-export async function generateStaticParams() {
-
-  const res = await fetch(`${BASE_URL}/api/products`, {
-    cache: "force-cache",
-  });
-  const products = await res.json();
-  return products.map((product: Product) => ({
-    id: product.id,
-  }));
-}
-
 export default async function ProductPage({
-  params
+  params,
 }: Readonly<{
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }>) {
   const { id } = await params;
 
-  const res = await fetch(`${BASE_URL}/api/products`, {
-    cache: "force-cache",
+  const res = await fetch(`${BASE_URL}}/api/products`, {
+    cache: "no-store",
   });
+
+  if (!res.ok) {
+    console.error(`Failed to fetch products: ${res.status} ${res.statusText}`);
+    notFound();
+  }
+
   const products = await res.json();
   const product = products.find((p: Product) => p.id === id);
 
